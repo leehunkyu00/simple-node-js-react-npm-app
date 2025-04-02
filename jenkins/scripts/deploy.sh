f546f36b9c1cda6557552d24bc4c64257f5008e3
hklee <leehunkyu00@gmail.com>
2025-04-02 10:31:39 +0900

    modify deploy.sh mode

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -5,13 +5,13 @@ chmod +x deploy.sh
 
 echo 'Deploying application...'
 
-# /var/test 디렉토리가 없는 경우 생성
-sudo mkdir -p /var/test
+# Mac OS에서 사용자 홈 디렉토리 아래에 deploy 폴더 생성
+mkdir -p ~/jenkins-deploy
 
-# 빌드된 파일을 /var/test 디렉토리로 복사
-sudo cp -r build/* /var/test/
+# 빌드된 파일을 복사
+cp -r build/* ~/jenkins-deploy/
 
 # 또는 사용자 지정 디렉토리로 복사
 # sudo cp -r build/* /path/to/your/deploy/directory/
 
-echo 'Application deployed successfully to /var/test directory' 
+echo 'Application deployed successfully to ~/jenkins-deploy directory' 
